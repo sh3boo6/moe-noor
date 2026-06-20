e143f2739895f959b5a0d3540b0882dd3cf0e9f6
MOHAMMED A ALOMARI <mohammedaalomari@sh3boo6s-MacBook-Pro.local>
2026-06-20 21:12:57 +0300

    update PWA

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -41,20 +41,60 @@ export default defineNuxtConfig({
     registerWebManifestInRouteRules: true,
     injectRegister: 'inline',
     manifest: {
-      name: 'Nuxt 4 PWA',
-      short_name: 'NuxtPWA',
-      description: 'A Nuxt 4 Progressive Web App',
-      theme_color: '#4A90E2',
+      name: 'وزارة التعليم',
+      short_name: 'وزارة التعليم',
+      description: 'تطبيق ويب تقدمي لوزارة التعليم',
+      theme_color: '#179f64',
       icons: [
         {
-          src: 'icons/icon-192.png',
-          sizes: '192x192',
-          type: 'image/png'
+          "src": "icons/icon-48x48.png",
+          "sizes": "48x48",
+          "type": "image/png"
         },
         {
-          src: 'icons/icon-512.png',
-          sizes: '512x512',
-          type: 'image/png'
+          "src": "icons/icon-72x72.png",
+          "sizes": "72x72",
+          "type": "image/png"
+        },
+        {
+          "src": "icons/icon-96x96.png",
+          "sizes": "96x96",
+          "type": "image/png"
+        },
+        {
+          "src": "icons/icon-128x128.png",
+          "sizes": "128x128",
+          "type": "image/png"
+        },
+        {
+          "src": "icons/icon-144x144.png",
+          "sizes": "144x144",
+          "type": "image/png"
+        },
+        {
+          "src": "icons/icon-152x152.png",
+          "sizes": "152x152",
+          "type": "image/png"
+        },
+        {
+          "src": "icons/icon-192x192.png",
+          "sizes": "192x192",
+          "type": "image/png"
+        },
+        {
+          "src": "icons/icon-256x256.png",
+          "sizes": "256x256",
+          "type": "image/png"
+        },
+        {
+          "src": "icons/icon-384x384.png",
+          "sizes": "384x384",
+          "type": "image/png"
+        },
+        {
+          "src": "icons/icon-512x512.png",
+          "sizes": "512x512",
+          "type": "image/png"
         }
       ]
     },
